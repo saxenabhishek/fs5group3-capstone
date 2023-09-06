5843d30d806125b56f4a3c23a914be7c5a988d54
Associate <Associate@roifmrwindt>
2023-09-06 10:32:22 +0530

    feat: price model

--- src/app/models/instruments.ts
+++ src/app/models/instruments.ts
@@ -3,8 +3,8 @@ export class Instruments {
     public instrumentId: String,
     public externalIdType: String,
     public externalId: String,
-    public categoryId: String,
-    public instrumentDescription: String,
+    public categoryId: string,
+    public instrumentDescription: string,
     public maxQuantity: Number,
     public minQuantity: Number
   ) {}
