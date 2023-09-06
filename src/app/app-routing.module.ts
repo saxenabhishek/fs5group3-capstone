c1f5e801682f27b4bb0f7635202009b3b57fd6d4
Associate <Associate@roifmrwindt>
2023-09-06 13:00:42 +0530

    added login page html, css, TS file and updated the routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterPageComponent } from './register-page/register-page.component';
 import { DemoLandingComponent } from './demo-landing/demo-landing.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 const routes: Routes = [
   {path:'register',component:RegisterPageComponent},
+  {path:'login',component:LoginPageComponent},
   {path:'',component: DemoLandingComponent}
 ];
 
--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
-export class LoginPageComponent {
-  
+export class LoginPageComponent  {
+  loginForm=new FormGroup({
+    email:new FormControl('',[ Validators.required,Validators.maxLength(50),Validators.email]),
+    password:new FormControl('', [Validators.required,Validators.pattern(
+      '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,12}$'
+    )])
+  });
+  submitted = false;
 
-}
+  get loginData(){
+    return this.loginForm.controls;
+  }
+  onSubmit() {
+    this.submitted = true;
+    console.log(this.loginForm);
+
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+
+        return;
+    }
+  }
+  
+}//
