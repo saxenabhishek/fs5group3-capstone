04f14803eb27e83967a1dbbd55a73ecd0a850fec
Associate <Associate@roifmrwindt>
2023-09-13 10:26:40 +0530

    Created models

--- JavaBackend/src/main/java/models/Order.java
+++ JavaBackend/src/main/java/models/Order.java
@@ -1,5 +1,83 @@
 package models;
 
-public class Order {
+import java.math.BigDecimal;
+import java.time.Instant;
 
+public class Order {
+	private String instrumentId, clientId, orderId;
+	private long quantity;
+	private BigDecimal targetPrice;
+	private Direction direction;
+	private Instant timestamp;
+	
+	public Order (String instrumentId, long quantity, BigDecimal targetPrice, String direction,
+					String clientId, String orderId, Instant timestamp) 
+	{
+		if (instrumentId == null)
+			throw new NullPointerException ("Instrument ID can't be null");
+		if (instrumentId == "")
+			throw new IllegalArgumentException ("Instrument ID can't be empty");
+		this.instrumentId = instrumentId;
+		
+		if (clientId == null)
+			throw new NullPointerException ("Client ID can't be null");
+		if (clientId == "")
+			throw new IllegalArgumentException ("Client ID can't be empty");
+		this.clientId = clientId;
+		
+		if (orderId == null)
+			throw new NullPointerException ("Order ID can't be null");
+		if (orderId == "")
+			throw new IllegalArgumentException ("Order ID can't be empty");
+		this.orderId = orderId;
+		
+		if (quantity <= 0)
+			throw new IllegalArgumentException ("Order quantity can't be zero or negative");		
+		this.quantity = quantity;
+		
+		if (targetPrice.compareTo(BigDecimal.ZERO) <= 0)
+			throw new IllegalArgumentException ("Target price of order can't be zero or negative");	
+		this.targetPrice = targetPrice;
+		
+		if ("B".equals(direction)) 
+            this.direction = Direction.BUY;
+        else if ("S".equals(direction)) 
+            this.direction = Direction.SELL;
+        else if (direction == null)
+            throw new NullPointerException("Order direction value can't be null");
+        else
+            throw new IllegalArgumentException("Invalid order direction value! Only B(for Buy) or S (for Sell)");
+	
+		if (timestamp== null)
+			throw new NullPointerException ("Order timestamp can't be null");
+		this.timestamp= timestamp;
+	}
+
+	public String getInstrumentId() {
+		return this.instrumentId;
+	}
+
+	public String getClientId() {
+		return this.clientId;
+	}
+
+	public String getOrderId() {
+		return this.orderId;
+	}
+
+	public long getQuantity() {
+		return this.quantity;
+	}
+
+	public BigDecimal getTargetPrice() {
+		return this.targetPrice;
+	}
+
+	public Direction getDirection() {
+		return this.direction;
+	}	
+	
+	public Instant getTimestamp() {
+		return this.timestamp;
+	}
 }
--- JavaBackend/src/main/java/models/Trade.java
+++ JavaBackend/src/main/java/models/Trade.java
@@ -1,5 +1,76 @@
 package models;
 
-public class Trade {
+import java.math.BigDecimal;
+import java.time.Instant;
 
+public class Trade {
+	private Order order;
+	private BigDecimal cashValue, executionPrice;
+	private long quantity;
+	private Direction direction;
+	private String instrumentId, clientId, tradeId;
+	private Instant timestamp;
+	
+	public Trade(Order order, BigDecimal executionPrice, String tradeId, Instant timestamp) {
+		
+		if (order == null)
+			throw new NullPointerException ("Order can't be null");
+		this.order = order;		
+		
+		if (executionPrice.compareTo(BigDecimal.ZERO) <= 0)
+			throw new IllegalArgumentException ("Execution price of order can't be zero or negative");	
+		this.executionPrice = executionPrice;
+		
+		this.quantity = order.getQuantity();
+		this.cashValue = executionPrice.multiply(new BigDecimal(quantity));		
+        this.direction = order.getDirection();
+		this.instrumentId = order.getInstrumentId();
+		this.clientId = order.getClientId();
+		
+		if (tradeId == null)
+			throw new NullPointerException ("Trade ID can't be null");
+		if (tradeId == "")
+			throw new IllegalArgumentException ("Trade ID can't be empty");
+		this.tradeId = tradeId;
+		
+		if (timestamp== null)
+			throw new NullPointerException ("Trade timestamp can't be null");
+		this.timestamp= timestamp;
+	}
+
+	public Order getOrder() {
+		return this.order;
+	}
+
+	public BigDecimal getCashValue() {
+		return this.cashValue;
+	}
+
+	public BigDecimal getExecutionPrice() {
+		return this.executionPrice;
+	}
+
+	public long getQuantity() {
+		return this.quantity;
+	}
+
+	public Direction getDirection() {
+		return this.direction;
+	}
+
+	public String getInstrumentId() {
+		return this.instrumentId;
+	}
+
+	public String getClientId() {
+		return this.clientId;
+	}
+
+	public String getTradeId() {
+		return this.tradeId;
+	}
+
+	public Instant getTimestamp() {
+		return this.timestamp;
+	}	
 }
