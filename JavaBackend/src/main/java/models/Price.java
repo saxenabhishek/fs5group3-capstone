58cc802d1de91d0919cb6a4fc99327463d718f35
Associate <Associate@roifmrwindt>
2023-09-13 13:48:12 +0530

    feat: price

--- JavaBackend/src/main/java/models/Price.java
+++ JavaBackend/src/main/java/models/Price.java
@@ -1,5 +1,20 @@
 package models;
 
-public class Price {
+import java.time.LocalDateTime;
 
+public class Price {
+	public Double askPrice;
+	public Double bidPrice;
+	public String instrumentId;
+	public LocalDateTime priceTimestamp;
+	public Instrument instrument;
+	
+	public Price(Instrument instrument, Double askPrice, String instrumentId ,Double bidPrice, LocalDateTime priceTimestamp) {
+		super();
+		this.askPrice = askPrice;
+		this.bidPrice = bidPrice;
+		this.priceTimestamp = priceTimestamp;
+		this.instrument = instrument;
+		this.instrumentId = instrumentId;
+	}
 }
