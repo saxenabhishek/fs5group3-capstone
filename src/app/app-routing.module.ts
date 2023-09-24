ab0877b21e375b9f9b2bc41478b81e97efe89f54
Associate <Associate@roifmrwindt>
2023-09-24 22:54:36 +0530

    feat: add route guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { RegisterPageComponent } from './register-page/register-page.component';
-import { StockPageComponent } from './stock-page/stock-page.component';
+import { NgModule, inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivateFn, RouterModule, RouterStateSnapshot, Routes, UrlSerializer } from '@angular/router';
 import { ExplorePageComponent } from './explore-page/explore-page.component';
+import { InvestmentPreferUpdateComponent } from './investment-prefer-update/investment-prefer-update.component';
+import { InvestmentPreferComponent } from './investment-prefer/investment-prefer.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
-import { TradeHistoryComponent } from './trade-history/trade-history.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 import { ReportPageComponent } from './report-page/report-page.component';
 import { RoboadvisorPageComponent } from './roboadvisor-page/roboadvisor-page.component';
-import { InvestmentPreferComponent } from './investment-prefer/investment-prefer.component';
-import { InvestmentPreferUpdateComponent } from './investment-prefer-update/investment-prefer-update.component';
+import { ClientService } from './services/client/client.service';
+import { StockPageComponent } from './stock-page/stock-page.component';
+import { TradeHistoryComponent } from './trade-history/trade-history.component';
+
+
+const routeGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state:RouterStateSnapshot) => {
+  return inject(ClientService).getIfLoggedIn() || inject(UrlSerializer).parse("/");
+};
 
 const routes: Routes = [
   {
@@ -28,10 +34,12 @@ const routes: Routes = [
   {
     path: 'portfolio',
     component: PortfolioComponent,
+    canActivate:[routeGuard]
   },
   {
     path: 'preferences/add',
     component: InvestmentPreferComponent,
+    
   },
   {
     path: 'preferences/update',
@@ -40,14 +48,20 @@ const routes: Routes = [
   {
     path: 'trade-history',
     component: TradeHistoryComponent,
+    canActivate:[routeGuard]
+
   },
   {
     path: 'report',
     component: ReportPageComponent,
+    canActivate:[routeGuard]
+
   },
   {
     path: 'roboadvisor',
     component: RoboadvisorPageComponent,
+    canActivate:[routeGuard]
+
   },
   { 
     path: 'explore', 
