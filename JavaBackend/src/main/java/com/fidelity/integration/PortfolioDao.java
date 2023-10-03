8d2ff40e983b8f049d8585fa0d9630f7216e4afc
Associate <Associate@roifmrwindt>
2023-10-03 19:33:46 +0530

    refactor: remove dead code

--- JavaBackend/src/main/java/com/fidelity/integration/PortfolioDao.java
+++
@@ -1,9 +0,0 @@
-package com.fidelity.integration;
-
-import java.util.List;
-
-import com.fidelity.models.Trade;
-
-public interface PortfolioDao {
-	List<Trade> getPortfolio(String clientId);
-}
--- JavaBackend/src/test/java/com/fidelity/models/PriceTest.java
+++
@@ -1,5 +0,0 @@
-package com.fidelity.models;
-
-public class PriceTest {
-
-}
