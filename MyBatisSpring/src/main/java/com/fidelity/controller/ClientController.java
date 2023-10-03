376851a775f9daa627e23a7bec20c2afa5f9801e
Associate <Associate@roifmrwindt>
2023-10-03 19:50:46 +0530

    docs: add spring boot and controllers

---
+++ MyBatisSpring/src/main/java/com/fidelity/controller/ClientController.java
@@ -0,0 +1,17 @@
+package com.fidelity.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/client")
+public class ClientController {
+    
+    @PostMapping("/register")
+    ResponseEntity<Integer> registerNewClient(){
+        throw new java.lang.UnsupportedOperationException();
+    }
+}
+
