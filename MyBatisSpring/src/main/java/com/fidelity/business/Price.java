69e10eccfda31a65c3ce60f899cd7c02160b0c51
Associate <Associate@roifmrwindt>
2023-10-08 23:15:53 +0530

    feat: use lombok to set getters and setters

--- MyBatisSpring/src/main/java/com/fidelity/business/Price.java
+++ MyBatisSpring/src/main/java/com/fidelity/business/Price.java
@@ -1,67 +1,29 @@
 package com.fidelity.business;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter
+@ToString
+@NoArgsConstructor
 public class Price {
-	public BigDecimal askPrice;
-	public BigDecimal bidPrice;
-	public String instrumentId;
-	public LocalDate priceTimestamp;
-	public Instrument instrument;
+	private BigDecimal askPrice;
+	private BigDecimal bidPrice;
+	private String instrumentId;
+	private LocalDateTime priceTimestamp;
+	private Instrument instrument;
 	
-	public Price(Instrument instrument, BigDecimal askPrice, String instrumentId ,BigDecimal bidPrice, LocalDate priceTimestamp) {
-		super();
-		this.askPrice = askPrice;
-		this.bidPrice = bidPrice;
-		this.priceTimestamp = priceTimestamp;
-		this.instrument = instrument;
-		this.instrumentId = instrumentId;
-	}
-
-	public BigDecimal getAskPrice() {
-		return askPrice;
-	}
-
-	public void setAskPrice(BigDecimal askPrice) {
-		this.askPrice = askPrice;
-	}
-
-	public BigDecimal getBidPrice() {
-		return bidPrice;
-	}
-
-	public void setBidPrice(BigDecimal bidPrice) {
-		this.bidPrice = bidPrice;
-	}
-
-	public String getInstrumentId() {
-		return instrumentId;
-	}
-
-	public void setInstrumentId(String instrumentId) {
-		this.instrumentId = instrumentId;
-	}
-
-	public LocalDate getPriceTimestamp() {
-		return priceTimestamp;
-	}
-
-	public void setPriceTimestamp(LocalDate priceTimestamp) {
-		this.priceTimestamp = priceTimestamp;
-	}
-
-	public Instrument getInstrument() {
-		return instrument;
-	}
-
-	public void setInstrument(Instrument instrument) {
-		this.instrument = instrument;
-	}
-
-	@Override
-	public String toString() {
-		return "Price [askPrice=" + askPrice + ", bidPrice=" + bidPrice + ", instrumentId=" + instrumentId
-				+ ", priceTimestamp=" + priceTimestamp + ", instrument=" + instrument + "]";
-	}
+	// public Price(Instrument instrument, BigDecimal askPrice, String instrumentId ,BigDecimal bidPrice, Instant priceTimestamp) {
+	// 	super();
+	// 	this.askPrice = askPrice;
+	// 	this.bidPrice = bidPrice;
+	// 	this.priceTimestamp = priceTimestamp;
+	// 	this.instrument = instrument;
+	// 	this.instrumentId = instrumentId;
+	// }
 }
