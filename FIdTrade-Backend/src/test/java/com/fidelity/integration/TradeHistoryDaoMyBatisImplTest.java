365067eeae005d742178dc6387f5bc79209cd5bf
Associate <Associate@roifmrwindt>
2023-10-09 10:18:15 +0530

    feat: trade controller and get instruments

--- FIdTrade-Backend/src/test/java/com/fidelity/integration/TradeHistoryDaoMyBatisImplTest.java
+++ FIdTrade-Backend/src/test/java/com/fidelity/integration/TradeHistoryDaoMyBatisImplTest.java
@@ -1,6 +1,5 @@
 package com.fidelity.integration;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -20,19 +19,12 @@ class TradeHistoryDaoMyBatisImplTest {
 	@Autowired
 	private TradeHistoryDaoMyBatisImpl dao;
 
-//	private List<Order> tradehistoryonly = Arrays.asList(
-//			
-//            new Order("T67878", 8000, new BigDecimal("0.98546875"), "BUY", "UID001", "OID002",Instant.parse("2022-04-20T10:15:00.00Z")
-//
-//		    ));
 	@Test
 	void testSizeGetTradeHistory() {
-			List<Order> orders = dao.getAllTradeHistory();
+		List<Order> orders = dao.getAllTradeHistory();
 		assertNotNull(orders);
 		assertEquals(orders.size(), 4);
-		
-	}
-	
 
-	
+	}
+
 }
