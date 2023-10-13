f38a5dbdd1230265a2e563b8062e7930a2ff5732
Associate <Associate@roifmrwindt>
2023-10-14 03:25:02 +0530

    Deleted getPriceBYId

--- src/app/services/trade/trade.service.ts
+++ src/app/services/trade/trade.service.ts
@@ -2,11 +2,11 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, catchError, of, throwError } from 'rxjs';
 import { INSTRUMENT_DATA } from 'src/app/const/instrument';
-import { PRICES } from 'src/app/const/prices';
 import { Instruments } from 'src/app/models/instruments';
 import { Order } from 'src/app/models/order';
 import { Prices } from 'src/app/models/prices';
 import { Trade } from 'src/app/models/trade';
+import { ClientService } from '../client/client.service';
 
 @Injectable({
   providedIn: 'root',
@@ -16,7 +16,7 @@ export class TradeService {
   url = 'http://localhost:8080/trade';
   portfolioUrl= 'http://localhost:8080/portfolio';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private clientService: ClientService) {}
 
   processOrder(order: Order) {
     // todo: make call to backend to process an order and make a trade
@@ -101,21 +101,6 @@ export class TradeService {
     let ele: Instruments = new Instruments('', '', '', '', '', 0, 0);
     INSTRUMENT_DATA.forEach((element) => {
       if (element.instrumentId == id) {
-        ele = element;
-      }
-    });
-    return of(ele);
-  }
-
-  getPriceById(id: String): Observable<Prices> {
-    let ele: Prices = new Prices(
-      0,
-      0,
-      '',
-      new Instruments('', '', '', '', '', 0, 0)
-    );
-    PRICES.forEach((element) => {
-      if (element.instrument.instrumentId == id) {
         ele = element;
       }
     });
