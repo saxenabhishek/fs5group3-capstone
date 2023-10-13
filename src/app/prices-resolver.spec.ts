ddb792b940db64a0fc51fadd51aa000390d8e9c9
Associate <Associate@roifmrwindt>
2023-10-14 03:12:15 +0530

    Price Resolver Added

---
+++ src/app/prices-resolver.spec.ts
@@ -0,0 +1,7 @@
+import { PricesResolver } from './prices-resolver';
+
+describe('PricesResolver', () => {
+  it('should create an instance', () => {
+    expect(new PricesResolver()).toBeTruthy();
+  });
+});
